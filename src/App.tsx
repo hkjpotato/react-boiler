960e46ea03f4f2325c518b4d787de326f6c25326
hkjpotato <hkjpotato@gmail.com>
2021-10-27 10:47:07 -0400

    init ts trial

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type AppProps = {
+  appName: string,
+}
+
+const App:React.FunctionComponent<AppProps> =  ({ appName }) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello ts react</h1>
+        <h1>hello ts react {name}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
